3b13094513e3e197bbaf21e9ee6edf5cc71d956f
KhizarJamshaidIqbal <112169672+KhizarJamshaidIqbal@users.noreply.github.com>
2022-10-19 06:41:21 -0700

    color change

--- lib/screen/Foo_Animated_Container.dart
+++ lib/screen/Foo_Animated_Container.dart
@@ -54,7 +54,7 @@ class _Foo_Animated_ContainerState extends State<Foo_Animated_Container> {
                       // bgColor = Colors.blueGrey;
                       myDecor = BoxDecoration(
                         borderRadius: BorderRadius.circular(10),
-                        color: Colors.redAccent,
+                        color: Colors.bluegrey,
                       );
                       flag = true;
                     }
